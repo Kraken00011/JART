5843d9b3a2f09f5ed90193b3c9657b73aec68e22
Kraken <99017522+Kraken00011@users.noreply.github.com>
2026-06-20 02:57:22 +0600

    Добавление предосмотра бонусов Clamity

---
+++ tRUCompat/ArmorSetBonusesPreview/ClamitySetBonuses.cs
@@ -0,0 +1,27 @@
+using System;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using CalamityMod;
+
+internal class ClamitySetBonuses : ModSystem
+{
+    public override void PostSetupContent()
+    {
+        ModLoader.TryGetMod("CalamityRuTranslate", out Mod tru);
+        ModLoader.TryGetMod("Clamity", out Mod clam);
+
+        if (tru != null && clam != null)
+        {
+            tru.Call("AddArmorSetBonusPreview", clam.Find<ModItem>("FrozenHellstoneVisor").Type, (Func<string>)(() =>
+            {
+                string hotkey = CalamityUtils.TooltipHotkeyString(CalamityKeybinds.ArmorSetBonusHotKey);
+                return Language.GetTextValue("Mods.Clamity.Items.Armor.FrozenHellstone.FrozenHellstoneVisor.SetBonus", hotkey);
+            }));
+
+            tru.Call("AddArmorSetBonusPreview", clam.Find<ModItem>("ClamitasShellmet").Type, (Func<string>)(() =>
+            {
+                return Language.GetTextValue("Mods.Clamity.Items.Armor.Clamitas.ClamitasShellmet.SetBonus");
+            }));
+        }
+    }
+}
