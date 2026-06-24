81426353da35740c2f61a7da8f1ad013e1c5d0b0
Kraken <99017522+Kraken00011@users.noreply.github.com>
2026-06-24 16:37:12 +0600

    Create HMOreSummonerHelmetsSetBonuses.cs

---
+++ tRUCompat/ArmorSetBonusesPreview/HMOreSummonerHelmetsSetBonuses.cs
@@ -0,0 +1,40 @@
+using System;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using CalamityMod;
+
+internal class HMOreSummonerHelmetsSetBonuses : ModSystem
+{
+    public override void PostSetupContent()
+    {
+        ModLoader.TryGetMod("CalamityRuTranslate", out Mod tru);
+        ModLoader.TryGetMod("HMOreSummonerHelmets", out Mod hmOreHelms);
+
+        if (tru != null && hmOreHelms != null)
+        {
+            tru.Call("AddArmorSetBonusPreview", hmOreHelms.Find<ModItem>("AdamantiteHelm").Type, (Func<string>)(() =>
+                Language.GetTextValue("Mods.JAtRT.HMOreSummonerHelmets.ArmorSetBonuses.AdamantiteHelm")
+            ));
+
+            tru.Call("AddArmorSetBonusPreview", hmOreHelms.Find<ModItem>("OrichalcumCrown").Type, (Func<string>)(() =>
+                Language.GetTextValue("Mods.JAtRT.HMOreSummonerHelmets.ArmorSetBonuses.OrichalcumCrown")
+            ));
+
+            tru.Call("AddArmorSetBonusPreview", hmOreHelms.Find<ModItem>("CobaltKabuto").Type, (Func<string>)(() =>
+                Language.GetTextValue("Mods.JAtRT.HMOreSummonerHelmets.ArmorSetBonuses.CobaltKabuto")
+            ));
+
+            tru.Call("AddArmorSetBonusPreview", hmOreHelms.Find<ModItem>("MythrilCrown").Type, (Func<string>)(() =>
+                Language.GetTextValue("Mods.JAtRT.HMOreSummonerHelmets.ArmorSetBonuses.MythrilCrown")
+            ));
+
+            tru.Call("AddArmorSetBonusPreview", hmOreHelms.Find<ModItem>("PalladiumHelm").Type, (Func<string>)(() =>
+                Language.GetTextValue("Mods.JAtRT.HMOreSummonerHelmets.ArmorSetBonuses.PalladiumHelm")
+            ));
+
+            tru.Call("AddArmorSetBonusPreview", hmOreHelms.Find<ModItem>("TitaniumGrowth").Type, (Func<string>)(() =>
+                Language.GetTextValue("Mods.JAtRT.HMOreSummonerHelmets.ArmorSetBonuses.TitaniumGrowth")
+            ));
+        }
+    }
+}
