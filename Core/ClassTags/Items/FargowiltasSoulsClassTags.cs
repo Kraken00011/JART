7afbd0b501b88446b9df51c5d990262095435c2b
Kraken <99017522+Kraken00011@users.noreply.github.com>
2026-07-01 00:05:03 +0600

    Create FargowiltasSoulsClassTags.cs

---
+++ Core/ClassTags/Items/FargowiltasSoulsClassTags.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using System.Linq;
+using JAtRT.Core.ClassTags;
+
+public class FargowiltasSoulsClassTags : ItemTagsAdder
+{
+    public bool IsEnabled => ModLoader.HasMod("FargowiltasSouls") && Language.ActiveCulture.Name == "ru-RU";
+
+    public List<(int ItemType, string TagName)> TaggedItems
+    {
+        get
+        {
+            var result = new List<(int, string)>();
+
+            // Class Tags
+            string[] Warrior =
+            {
+                "BarbariansEssence",
+                "BerserkerSoul",
+                "BrokenBlade",
+                "BrokenHilt",
+                "BrokenSpearhead",
+                "SolarBooster"
+            };
+
+            string[] Ranger =
+            {
+                "SharpshootersEssence",
+                "SnipersSoul",
+                "VortexBooster"
+            };
+
+            string[] Sorcerer =
+            {
+                "ApprenticesEssence",
+                "ArchWizardsSoul",
+                "NebulaBooster"
+            };
+
+            string[] Summoner =
+            {
+                "OccultistsEssence",
+                "ConjuristsSoul",
+                "StardustBooster"
+            };
+
+            if (ModLoader.HasMod("FargowiltasSouls"))
+            {
+                result.AddRange(ClassTagsAdderHelper.GetTaggedItems(Warrior, "WarriorTag", "FargowiltasSouls"));
+                result.AddRange(ClassTagsAdderHelper.GetTaggedItems(Ranger, "RangerTag", "FargowiltasSouls"));
+                result.AddRange(ClassTagsAdderHelper.GetTaggedItems(Sorcerer, "SorcererTag", "FargowiltasSouls"));
+                result.AddRange(ClassTagsAdderHelper.GetTaggedItems(Summoner, "SummonerTag", "FargowiltasSouls"));
+            }
+
+            return result;
+        }
+    }
+}
