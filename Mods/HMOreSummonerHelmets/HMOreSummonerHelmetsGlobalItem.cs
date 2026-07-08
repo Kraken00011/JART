563241c5e18adc40582b2de194d55f19a4b650bf
Kraken <99017522+Kraken00011@users.noreply.github.com>
2026-07-08 17:54:31 +0600

    Update HMOreSummonerHelmetsGlobalItem.cs

--- Mods/HMOreSummonerHelmets/HMOreSummonerHelmetsGlobalItem.cs
+++ Mods/HMOreSummonerHelmets/HMOreSummonerHelmetsGlobalItem.cs
@@ -19,34 +19,22 @@ public partial class HMOreSummonerHelmetsGlobalItem : GlobalItem
                 string setBonusKey = Language.GetTextValue("LegacyTooltip.48");
 
                 if (tooltip.Text.Contains("Flower petals will fall on your target for extra damage"))
-                {
                     tooltip.Text = $"{setBonusKey} {Language.GetTextValue("Mods.JAtRT.HMOreSummonerHelmets.ArmorSetBonuses.OrichalcumCrown")}";
-                }
 
                 else if (tooltip.Text.Contains("Increases movement and whip speed by 25%"))
-                {
                     tooltip.Text = $"{setBonusKey} {Language.GetTextValue("Mods.JAtRT.HMOreSummonerHelmets.ArmorSetBonuses.CobaltKabuto")}";
-                }
 
                 else if (tooltip.Text.Contains("Greatly increases life regeneration after striking an enemy"))
-                {
                     tooltip.Text = $"{setBonusKey} {Language.GetTextValue("Mods.JAtRT.HMOreSummonerHelmets.ArmorSetBonuses.PalladiumHelm")}";
-                }
 
                 else if (tooltip.Text.Contains("Minions and sentries have a 20% chance to critically strike"))
-                {
                     tooltip.Text = $"{setBonusKey} {Language.GetTextValue("Mods.JAtRT.HMOreSummonerHelmets.ArmorSetBonuses.MythrilCrown")}";
-                }
 
                 else if (tooltip.Text.Contains("Attacking generates a defensive barrier of titanium shards"))
-                {
                     tooltip.Text = $"{setBonusKey} {Language.GetTextValue("Mods.JAtRT.HMOreSummonerHelmets.ArmorSetBonuses.TitaniumGrowth")}";
-                }
 
                 else if (tooltip.Text.Contains("Increases whip range by 25%"))
-                {
                     tooltip.Text = $"{setBonusKey} {Language.GetTextValue("Mods.JAtRT.HMOreSummonerHelmets.ArmorSetBonuses.AdamantiteHelm")}";
-                }
             }
         }
     }
