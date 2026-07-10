d176bfe3be9fe10b1f8e8ca219f0febf99820992
Kraken <99017522+Kraken00011@users.noreply.github.com>
2026-07-10 18:06:08 +0600

    Update ClassTagsAdder.cs

--- Core/ClassTags/ClassTagsAdder.cs
+++ Core/ClassTags/ClassTagsAdder.cs
@@ -15,19 +15,18 @@ namespace JAtRT.Core.ClassTags
 
         static ClassTagsAdder()
         {
-            var tags = new List<(string, string, Color)>
-            {
-                ("MulticlassTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Multiclass"), new Color(55, 182, 206)),
-                ("ClasslessTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Classless"), new Color(255, 255, 255)),
-                ("FishingTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Fishing"), new Color(102, 255, 255)),
-                ("MovementTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Movement"), new Color(144, 238, 144)),
-                ("SurvivabilityTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Survivability"), new Color(220, 20, 60)),
-                ("ToolTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Tool"), new Color(224, 255, 255)),
-                ("MiningTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Mining"), new Color(224, 255, 255)),
-            };
+            var tags = new List<(string, string, Color)>();
 
             if (ModLoader.HasMod("ThoriumClassTagsConsistency"))
             {
+                tags.Add(("MulticlassTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Multiclass"), new Color(55, 182, 206)));
+                tags.Add(("ClasslessTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Classless"), new Color(255, 255, 255)));
+                tags.Add(("FishingTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Fishing"), new Color(102, 255, 255)));
+                tags.Add(("MovementTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Movement"), new Color(144, 238, 144)));
+                tags.Add(("SurvivabilityTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Survivability"), new Color(220, 20, 60)));
+                tags.Add(("ToolTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Tool"), new Color(224, 255, 255)));
+                tags.Add(("MiningTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Mining"), new Color(224, 255, 255)));
+                
                 tags.Add(("WarriorTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Warrior"), new Color(229, 25, 25)));
                 tags.Add(("RangerTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Ranger"), new Color(0, 170, 56)));
                 tags.Add(("SorcererTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Sorcerer"), new Color(14, 129, 244)));
@@ -35,35 +34,35 @@ namespace JAtRT.Core.ClassTags
                 tags.Add(("ThrowerTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Thrower"), new Color(255, 165, 60)));
 
                 if (ModLoader.HasMod("CalamityMod"))
+                {
                     tags.Add(("TrueWarriorTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.TrueWarrior"), new Color(229, 25, 25)));
+                    tags.Add(("RogueTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Rogue"), new Color(255, 137, 0)));
+                }
+
+                if (ModLoader.HasMod("ThoriumMod"))
+                {
+                    tags.Add(("HealerTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Healer"), new Color(255, 255, 91)));
+                    tags.Add(("DarkHealerTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Healer"), new Color(178, 102, 255)));
+                    tags.Add(("BardTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Bard"), new Color(0, 255, 128)));
+                    tags.Add(("TransformationTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Transformation"), new Color(200, 175, 120)));
+                    tags.Add(("OmniShieldTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.OmniShield"), new Color(102, 255, 255)));
+                }
 
                 if (ModLoader.HasMod("Split"))
                     tags.Add(("HeavyWarriorTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.HeavyWarrior"), new Color(229, 25, 25)));
+
+                if (ModLoader.HasMod("SOTS"))
+                    tags.Add(("VoidTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Void"), new Color(153, 50, 204)));
+
+                if (ModLoader.HasMod("Redemption"))
+                    tags.Add(("RitualistTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Ritualist"), new Color(255, 69, 0)));
+
+                if (ModLoader.HasMod("DemolisherClass"))
+                    tags.Add(("DemolisherTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Demolisher"), new Color(246, 0, 24)));
+
+                if (ModLoader.HasMod("RoA"))
+                    tags.Add(("DruidTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Druid"), new Color(34, 139, 34)));
             }
-
-            if (ModLoader.HasMod("CalamityMod"))
-                tags.Add(("RogueTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Rogue"), new Color(255, 137, 0)));
-
-            if (ModLoader.HasMod("ThoriumMod"))
-            {
-                tags.Add(("HealerTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Healer"), new Color(255, 255, 91)));
-                tags.Add(("DarkHealerTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Healer"), new Color(178, 102, 255)));
-                tags.Add(("BardTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Bard"), new Color(0, 255, 128)));
-                tags.Add(("TransformationTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Transformation"), new Color(200, 175, 120)));
-                tags.Add(("OmniShieldTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.OmniShield"), new Color(102, 255, 255)));
-            }
-
-            if (ModLoader.HasMod("SOTS"))
-                tags.Add(("VoidTag", Language.GetTextValue("Mods.JAtRT.Tags.Special.Void"), new Color(153, 50, 204)));
-
-            if (ModLoader.HasMod("Redemption"))
-                tags.Add(("RitualistTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Ritualist"), new Color(255, 69, 0)));
-
-            if (ModLoader.HasMod("DemolisherClass"))
-                tags.Add(("DemolisherTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Demolisher"), new Color(246, 0, 24)));
-
-            if (ModLoader.HasMod("RoA"))
-                tags.Add(("DruidTag", Language.GetTextValue("Mods.JAtRT.Tags.Class.Druid"), new Color(34, 139, 34)));
 
             Tags = tags.ToArray();
         }
