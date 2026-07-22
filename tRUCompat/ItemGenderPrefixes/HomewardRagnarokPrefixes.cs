07a4b42deb18c03aae59d7dbf9d9fac4cd1e34d9
Kraken <99017522+Kraken00011@users.noreply.github.com>
2026-07-22 16:07:59 +0600

    Update HomewardRagnarokPrefixes.cs

--- tRUCompat/ItemGenderPrefixes/HomewardRagnarokPrefixes.cs
+++ tRUCompat/ItemGenderPrefixes/HomewardRagnarokPrefixes.cs
@@ -4,7 +4,7 @@ using JAtRT.Core.Config;
 
 internal class HomewardRagnarokPrefixes : ModSystem
 {
-    public override bool IsLoadingEnabled(Mod mod) => ModLoader.HasMod("CalamityRuTranslate") && ModLoader.HasMod("HomewardRagnarok") && JARTLocalizationConf.Instance.HomewardRagnarokLocalization && Language.ActiveCulture.Name == "ru-RU";
+    public override bool IsLoadingEnabled(Mod mod) => ModLoader.HasMod("CalamityRuTranslate") && ModLoader.HasMod("HomewardRagnarok") && Language.ActiveCulture.Name == "ru-RU";
     public override void PostSetupContent()
     {
         ModLoader.TryGetMod("CalamityRuTranslate", out Mod tru);
